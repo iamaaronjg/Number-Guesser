3d8bf8fa511ca05cc6aad20d2ab0977b6a798036
iamaaronjg <iamaaronjg@outlook.com>
2020-08-10 15:23:28 +0100

    Added validation logic and styling in app.js

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('app.js linked');
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain number of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if they lose
+- Let player choose to play again
+*/
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max values (initially placed in the HTML spans but removed after assignment)
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function() {
+    let guess = parseInt(guessInput.value);
+
+    // Validate user input
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+});
+
+// Set message
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+};
